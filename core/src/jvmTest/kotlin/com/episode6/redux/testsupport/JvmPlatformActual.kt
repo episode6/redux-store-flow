2ea88b593ae9e811c4cd86fe7136539fd1751a71
Geoff Hackett <ghackett@gmail.com>
2022-03-06 19:53:48 -0500

    i was doing it wrong

--- core/src/jvmTest/kotlin/com/episode6/redux/testsupport/JvmPlatformActual.kt
+++ core/src/jvmTest/kotlin/com/episode6/redux/testsupport/JvmPlatformActual.kt
@@ -2,14 +2,12 @@ package com.episode6.redux.testsupport
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.UncompletedCoroutinesError
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runBlockingTest
 
 @OptIn(ExperimentalCoroutinesApi::class)
-actual fun runTest(testBody: suspend CoroutineScope.() -> Unit) {
-  try {
-    runBlockingTest { testBody() }
-  } catch (e: UncompletedCoroutinesError) {
-    // no-op, store launches a coroutine on init
-  }
+actual fun runTest(testBody: suspend CoroutineScope.() -> Unit) = runBlockingTest {
+  val job = launch(block = testBody)
+  job.cancelAndJoin()
 }
