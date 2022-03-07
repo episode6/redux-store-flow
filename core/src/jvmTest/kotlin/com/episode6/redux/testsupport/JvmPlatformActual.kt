eb8e80c8b6099c93119d1da406dcaca7f725cbcf
Geoff Hackett <ghackett@gmail.com>
2022-03-06 21:01:11 -0500

    import

--- core/src/jvmTest/kotlin/com/episode6/redux/testsupport/JvmPlatformActual.kt
+++ core/src/jvmTest/kotlin/com/episode6/redux/testsupport/JvmPlatformActual.kt
@@ -1,6 +1,5 @@
 package com.episode6.redux.testsupport
 
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.launch
