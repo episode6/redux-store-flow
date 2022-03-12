bc011d4f96b1c5ed341d837f8eeb1c6c725f8f2c
Geoff Hackett <ghackett@gmail.com>
2022-03-12 14:46:55 -0500

    replace mock middleware test with better alt

--- core/src/commonTest/kotlin/com/episode6/redux/BeforeAndAfterMiddlewareTest.kt
+++ core/src/commonTest/kotlin/com/episode6/redux/BeforeAndAfterMiddlewareTest.kt
@@ -9,49 +9,86 @@ import io.mockk.*
 import kotlinx.coroutines.CoroutineScope
 import kotlin.test.Test
 
-private typealias StateAndAction = Pair<StopLightState, Action>
-
-private interface CaptureState {
-  fun capture(stateAndAction: StateAndAction)
-}
 
 class BeforeAndAfterMiddlewareTest {
-  private val beforeSlot = slot<StateAndAction>()
-  private val afterSlot = slot<StateAndAction>()
-  private val before: CaptureState = mockk {
-    every { capture(capture(beforeSlot)) } answers { }
-  }
-  private val after: CaptureState = mockk {
-    every { capture(capture(afterSlot)) } answers { }
-  }
-  private val middleware: Middleware<StopLightState> = Middleware { store, next ->
-    { action ->
-      before.capture(store.value to action)
-      next(action)
-      after.capture(store.value to action)
-    }
-  }
-
-  private fun CoroutineScope.stopLightStore(): StoreFlow<StopLightState> = createStopLightStore(middleware)
+  private fun CoroutineScope.stopLightStore(vararg middlewares: Middleware<StopLightState>): StoreFlow<StopLightState> =
+    createStopLightStore(*middlewares)
 
   @Test fun testProcessAction() = runTest {
-    val store = stopLightStore()
+    val middleware = TestMiddleware()
+    val store = stopLightStore(middleware)
     val action = SetRedLightOn(false)
 
     store.dispatch(action)
 
     verifyOrder {
-      before.capture(any())
-      after.capture(any())
+      middleware.before.captureState(any())
+      middleware.after.captureState(any())
     }
-    confirmVerified(before, after)
-    val (beforeState, beforeAction) = beforeSlot.captured
-    val (afterState, afterAction) = afterSlot.captured
+    confirmVerified(middleware.before, middleware.after)
+    val (beforeState, beforeAction) = middleware.beforeSlot.first()
+    val (afterState, afterAction) = middleware.afterSlot.first()
     assertAll {
       assertThat(beforeState).hasDefaultLights()
       assertThat(afterState).hasLights()
       assertThat(beforeAction).isEqualTo(action)
       assertThat(afterAction).isEqualTo(action)
+    }
+  }
+
+  @Test fun testProcessAction_twoMiddlewares() = runTest {
+    val middleware1 = TestMiddleware()
+    val middleware2 = TestMiddleware()
+    val store = stopLightStore(middleware1, middleware2)
+    val action = SetRedLightOn(false)
+
+    store.dispatch(action)
+
+    verifyOrder {
+      middleware1.before.captureState(any())
+      middleware2.before.captureState(any())
+      middleware2.after.captureState(any())
+      middleware1.after.captureState(any())
+    }
+    confirmVerified(middleware1.before, middleware1.after, middleware2.before, middleware2.after)
+    val (beforeState1, beforeAction1) = middleware1.beforeSlot.first()
+    val (afterState1, afterAction1) = middleware1.afterSlot.first()
+    val (beforeState2, beforeAction2) = middleware2.beforeSlot.first()
+    val (afterState2, afterAction2) = middleware2.afterSlot.first()
+    assertAll {
+      assertThat(beforeState1).hasDefaultLights()
+      assertThat(afterState1).hasLights()
+      assertThat(beforeAction1).isEqualTo(action)
+      assertThat(afterAction1).isEqualTo(action)
+      assertThat(beforeState2).hasDefaultLights()
+      assertThat(afterState2).hasLights()
+      assertThat(beforeAction2).isEqualTo(action)
+      assertThat(afterAction2).isEqualTo(action)
+    }
+  }
+}
+
+private data class StateAndAction(val state: StopLightState, val action: Action)
+
+private interface CaptureMiddlewareState {
+  fun captureState(stateAndAction: StateAndAction)
+}
+
+private class TestMiddleware : Middleware<StopLightState> {
+  val beforeSlot = mutableListOf<StateAndAction>()
+  val afterSlot = mutableListOf<StateAndAction>()
+  val before = mockk<CaptureMiddlewareState> {
+    every { captureState(capture(beforeSlot)) } answers {}
+  }
+  val after = mockk<CaptureMiddlewareState> {
+    every { captureState(capture(afterSlot)) } answers {}
+  }
+
+  override fun CoroutineScope.interfere(store: StoreFlow<StopLightState>, next: Dispatch): Dispatch {
+    return {
+      before.captureState(StateAndAction(store.value, it))
+      next(it)
+      after.captureState(StateAndAction(store.value, it))
     }
   }
 }
