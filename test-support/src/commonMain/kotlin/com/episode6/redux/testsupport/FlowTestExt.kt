951ded6ba5816098e2281a38f99152dbaa27b047
Geoff Hackett <ghackett@gmail.com>
2022-03-12 11:06:22 -0500

    clean up flow testing utils

--- test-support/src/commonMain/kotlin/com/episode6/redux/testsupport/FlowTestExt.kt
+++ test-support/src/commonMain/kotlin/com/episode6/redux/testsupport/FlowTestExt.kt
@@ -5,26 +5,23 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
+fun runFlowTest(testBody: suspend FlowTestScope.() -> Unit) = runTest { FlowTestScopeImpl(this).testBody() }
+
 interface FlowTestScope : CoroutineScope {
-  fun <T> Flow<T>.test(testBody: FlowValueCollector<T>.() -> Unit)
+  fun <T> Flow<T>.testCollector(): FlowValueCollector<T> = FlowValueCollectorImpl(this, this@FlowTestScope)
+  fun <T> Flow<T>.test(testBody: FlowValueCollector<T>.() -> Unit) = testCollector().testBody()
 }
 
 interface FlowValueCollector<T> {
   val values: List<T>
 }
 
-fun runFlowTest(testBody: suspend FlowTestScope.() -> Unit) = runTest { FlowTestScopeImpl(this).testBody() }
-
-private class FlowTestScopeImpl(private val delegate: CoroutineScope) : FlowTestScope, CoroutineScope by delegate {
-  override fun <T> Flow<T>.test(testBody: FlowValueCollector<T>.() -> Unit) {
-    val collector = FlowValueCollectorImpl<T>()
-    val job = launch { collect { collector._values.add(it) } }
-    collector.testBody()
-    job.cancel()
-  }
-}
-
-private class FlowValueCollectorImpl<T> : FlowValueCollector<T> {
-  val _values = mutableListOf<T>()
+private class FlowTestScopeImpl(private val delegate: CoroutineScope) : FlowTestScope, CoroutineScope by delegate
+private class FlowValueCollectorImpl<T>(flow: Flow<T>, scope: CoroutineScope) : FlowValueCollector<T> {
+  private val _values = mutableListOf<T>()
   override val values: List<T> get() = _values
+
+  init {
+    scope.launch { flow.collect { _values.add(it) } }
+  }
 }
