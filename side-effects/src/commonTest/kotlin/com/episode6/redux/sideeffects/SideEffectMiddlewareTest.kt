6d34e0fe2f8d07d7dfc2d1870f46ef8fdc84d703
Geoff Hackett <ghackett@gmail.com>
2022-03-13 16:32:31 -0400

    checkin

--- side-effects/src/commonTest/kotlin/com/episode6/redux/sideeffects/SideEffectMiddlewareTest.kt
+++ side-effects/src/commonTest/kotlin/com/episode6/redux/sideeffects/SideEffectMiddlewareTest.kt
@@ -1,15 +1,22 @@
-@file:OptIn(ExperimentalCoroutinesApi::class, FlowPreview::class)
+@file:OptIn(ExperimentalCoroutinesApi::class)
 
 package com.episode6.redux.sideeffects
 
+import assertk.all
+import assertk.assertThat
+import assertk.assertions.hasSize
+import assertk.assertions.index
 import com.episode6.redux.Action
 import com.episode6.redux.StoreFlow
 import com.episode6.redux.testsupport.TimingController
+import com.episode6.redux.testsupport.runFlowTest
+import com.episode6.redux.testsupport.runTest
 import com.episode6.redux.testsupport.stoplight.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.filterIsInstance
+import kotlinx.coroutines.flow.transformLatest
 import kotlin.test.Test
 
 class SideEffectMiddlewareTest {
@@ -17,46 +24,85 @@ class SideEffectMiddlewareTest {
   private val timing = TimingController()
   private fun CoroutineScope.stopLightStore() = stopLightStore(timing)
 
-  @Test fun testSomething() {
+  @Test fun testInitialValue() = runTest {
+    val store = stopLightStore()
 
+    assertThat(store.value).hasDefaultLights()
+  }
+
+  @Test fun testInitialValue_flow() = runFlowTest {
+    val store = stopLightStore()
+
+    store.test {
+      assertThat(values).all {
+        hasSize(1)
+        index(0).hasDefaultLights()
+      }
+    }
+  }
+
+  @Test fun testInitWithoutTime() = runTest {
+    val store = stopLightStore()
+
+    store.dispatch(SwitchToGreen)
+
+    assertThat(store.value).hasLights(green = true)
+  }
+
+  @Test fun testInitWithoutTime_flow() = runFlowTest {
+    val store = stopLightStore()
+
+    store.test {
+      store.dispatch(SwitchToGreen)
+
+      assertThat(values).all {
+        hasSize(3)
+        index(2).hasLights(green = true)
+      }
+    }
   }
 }
 
-private object ShutOffAllLights : Action
 private object SwitchToGreen : Action
 private object SwitchToYellow : Action
 private object SwitchToRed : Action
 
+private const val RED_TO_GREEN_DELAY = 75L
+private const val GREEN_TO_YELLOW_DELAY = 60L
+private const val YELLOW_TO_RED_DELAY = 15L
+
 private fun CoroutineScope.stopLightStore(timing: TimingController): StoreFlow<StopLightState> = createStopLightStore(
   SideEffectMiddleware(
     SideEffect {
-      actions.filterIsInstance<ShutOffAllLights>()
-        .map { flowOf(SetRedLightOn(false), SetGreenLightOn(false), SetYellowLightOn(false)) }
-        .flattenConcat()
-    },
-    SideEffect {
       actions.filterIsInstance<SwitchToGreen>().transformLatest {
-        emit(ShutOffAllLights)
-        emit(SetGreenLightOn(true))
-        timing.await(60)
+        emitLights(green = true)
+        timing.await(GREEN_TO_YELLOW_DELAY)
         emit(SwitchToYellow)
       }
     },
     SideEffect {
       actions.filterIsInstance<SwitchToYellow>().transformLatest {
-        emit(ShutOffAllLights)
-        emit(SetYellowLightOn(true))
-        timing.await(15)
+        emitLights(yellow = true)
+        timing.await(YELLOW_TO_RED_DELAY)
         emit(SwitchToRed)
       }
     },
     SideEffect {
       actions.filterIsInstance<SwitchToRed>().transformLatest {
-        emit(ShutOffAllLights)
-        emit(SetRedLightOn(true))
-        timing.await(75)
+        emitLights(red = true)
+        timing.await(RED_TO_GREEN_DELAY)
         emit(SwitchToGreen)
       }
     },
   )
 )
+
+private suspend fun FlowCollector<Action>.emitLights(
+  red: Boolean = false,
+  yellow: Boolean = false,
+  green: Boolean = false
+) {
+  emit(SetRedLightOn(red))
+  emit(SetYellowLightOn(yellow))
+  emit(SetGreenLightOn(green))
+}
