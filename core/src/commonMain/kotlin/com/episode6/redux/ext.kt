1f03962b3586636393099d50aaaa0100636081f5
Geoff Hackett <ghackett@gmail.com>
2022-03-13 08:31:27 -0400

    Stop exposing CoroutineScope (#8)

--- core/src/commonMain/kotlin/com/episode6/redux/ext.kt
+++ core/src/commonMain/kotlin/com/episode6/redux/ext.kt
@@ -7,7 +7,6 @@ fun <T, R> StoreFlow<T>.mapStore(mapper: (T)->R): StoreFlow<R> {
   val newInitialValue = mapper(initialValue)
   val newFlow = map { mapper(it) }.distinctUntilChanged()
   return object : StoreFlow<R>, Flow<R> by newFlow {
-    override val scope: CoroutineScope get() = this@mapStore.scope
     override val initialValue: R get() = newInitialValue
     override fun dispatch(action: Action) = this@mapStore.dispatch(action)
     override val replayCache: List<R> get() = this@mapStore.replayCache.map(mapper)
