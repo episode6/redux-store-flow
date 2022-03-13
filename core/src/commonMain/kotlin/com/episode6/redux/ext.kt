1326165d3cd37a45848cd4387a869864eb3ae027
Geoff Hackett <ghackett@gmail.com>
2022-03-12 20:00:41 -0500

    only create initial value once

--- core/src/commonMain/kotlin/com/episode6/redux/ext.kt
+++ core/src/commonMain/kotlin/com/episode6/redux/ext.kt
@@ -4,10 +4,11 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.*
 
 fun <T, R> StoreFlow<T>.mapStore(mapper: (T)->R): StoreFlow<R> {
-  val flow = map { mapper(it) }.stateIn(scope, SharingStarted.WhileSubscribed(), mapper(initialValue))
+  val newInitialValue = mapper(initialValue)
+  val flow = map { mapper(it) }.stateIn(scope, SharingStarted.WhileSubscribed(), newInitialValue)
   return object : StoreFlow<R>, StateFlow<R> by flow {
     override val scope: CoroutineScope get() = this@mapStore.scope
-    override val initialValue: R get() = mapper(this@mapStore.initialValue)
+    override val initialValue: R get() = newInitialValue
     override fun dispatch(action: Action) = this@mapStore.dispatch(action)
   }
 }
