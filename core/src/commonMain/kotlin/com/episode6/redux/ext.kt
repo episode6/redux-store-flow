b0c1fb329f598b9c132d27cb2389c54a1d7c4b7b
Geoff Hackett <ghackett@gmail.com>
2022-03-13 08:12:40 -0400

    Avoid using Flow.stateIn in StoreFlow.map

--- core/src/commonMain/kotlin/com/episode6/redux/ext.kt
+++ core/src/commonMain/kotlin/com/episode6/redux/ext.kt
@@ -5,10 +5,12 @@ import kotlinx.coroutines.flow.*
 
 fun <T, R> StoreFlow<T>.mapStore(mapper: (T)->R): StoreFlow<R> {
   val newInitialValue = mapper(initialValue)
-  val flow = map { mapper(it) }.stateIn(scope, SharingStarted.WhileSubscribed(), newInitialValue)
-  return object : StoreFlow<R>, StateFlow<R> by flow {
+  val newFlow = map { mapper(it) }.distinctUntilChanged()
+  return object : StoreFlow<R>, Flow<R> by newFlow {
     override val scope: CoroutineScope get() = this@mapStore.scope
     override val initialValue: R get() = newInitialValue
     override fun dispatch(action: Action) = this@mapStore.dispatch(action)
+    override val replayCache: List<R> get() = this@mapStore.replayCache.map(mapper)
+    override val value: R get() = mapper(this@mapStore.value)
   }
 }
