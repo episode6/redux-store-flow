3bfc92508f66fb4894d59a00f28788d1ae06e07e
Geoff Hackett <ghackett@gmail.com>
2022-03-19 11:50:13 -0400

    avoid stale emissions

--- subscriber-aware/src/commonMain/kotlin/com/episode6/redux/subscriberaware/SubscriberAwareStoreFlow.kt
+++ subscriber-aware/src/commonMain/kotlin/com/episode6/redux/subscriberaware/SubscriberAwareStoreFlow.kt
@@ -17,17 +17,21 @@ data class SubscriberStatusChanged(val subscribersActive: Boolean = false) : Act
  * collecting from it. The actions will fire when the first subscriber starts and the last subscriber
  * stops collecting.
  */
-@Suppress("FunctionName") fun <State: Any?> SubscriberAwareStoreFlow(
+@Suppress("FunctionName") fun <State : Any?> SubscriberAwareStoreFlow(
   scope: CoroutineScope,
   initialValue: State,
   reducer: Reducer<State>,
   middlewares: List<Middleware<State>> = emptyList(),
 ): StoreFlow<State> {
+
   val store = StoreFlow(scope = scope, initialValue = initialValue, reducer = reducer, middlewares = middlewares)
+
   val flow = store
+    .drop(1) // since we have to emit onStart, drop the store's first emission
     .onStart { store.dispatch(SubscriberStatusChanged(true)) }
-    .onCompletion { store.dispatch(SubscriberStatusChanged(false))  } 
-    .shareIn(scope, SharingStarted.WhileSubscribed(), replay = 1)
+    .onCompletion { store.dispatch(SubscriberStatusChanged(false)) }
+    .shareIn(scope, SharingStarted.WhileSubscribed(), replay = 0) // replay = stale emissions when SharingStarted.WhileSubscribed is used
+    .onStart { emit(store.state) } // replacement for replay
 
   return object : StoreFlow<State>, Flow<State> by flow {
     override val initialState: State get() = store.initialState
