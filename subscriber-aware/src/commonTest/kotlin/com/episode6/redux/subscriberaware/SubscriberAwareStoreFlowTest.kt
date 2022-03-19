e9d8df574d9bc070fb1d8d1264f9e4bda0498dc6
Geoff Hackett <ghackett@gmail.com>
2022-03-19 11:33:26 -0400

    BOOO shareIn emits stale data when resubscribing :(

--- subscriber-aware/src/commonTest/kotlin/com/episode6/redux/subscriberaware/SubscriberAwareStoreFlowTest.kt
+++ subscriber-aware/src/commonTest/kotlin/com/episode6/redux/subscriberaware/SubscriberAwareStoreFlowTest.kt
@@ -6,9 +6,9 @@ import assertk.assertions.*
 import com.episode6.redux.Action
 import com.episode6.redux.Middleware
 import com.episode6.redux.StoreFlow
+import com.episode6.redux.testsupport.lastElement
 import com.episode6.redux.testsupport.runFlowTest
-import com.episode6.redux.testsupport.stoplight.StopLightState
-import com.episode6.redux.testsupport.stoplight.reduce
+import com.episode6.redux.testsupport.stoplight.*
 import kotlinx.coroutines.CoroutineScope
 import kotlin.test.Test
 
@@ -53,6 +53,69 @@ class SubscriberAwareStoreFlowTest {
       hasSize(2)
       index(0).isEqualTo(SubscriberStatusChanged(subscribersActive = true))
       index(1).isEqualTo(SubscriberStatusChanged(subscribersActive = false))
+    }
+  }
+
+  @Test fun testMultipleSubscribers() = runFlowTest {
+    val store = stopLightStore()
+
+    val collector1 = store.testCollector()
+    val collector2 = store.testCollector()
+
+    assertThat(actions).all {
+      hasSize(1)
+      index(0).isEqualTo(SubscriberStatusChanged(subscribersActive = true))
+    }
+  }
+
+  @Test fun testDanglingSubscriber() = runFlowTest {
+    val store = stopLightStore()
+
+    val collector1 = store.testCollector()
+    val collector2 = store.testCollector()
+    collector1.stopCollecting()
+
+    assertThat(actions).all {
+      hasSize(1)
+      index(0).isEqualTo(SubscriberStatusChanged(subscribersActive = true))
+    }
+  }
+
+  @Test fun testMultipleSubscribers_unsubscribe() = runFlowTest {
+    val store = stopLightStore()
+
+    val collector1 = store.testCollector()
+    val collector2 = store.testCollector()
+    collector1.stopCollecting()
+    collector2.stopCollecting()
+
+    assertThat(actions).all {
+      hasSize(2)
+      index(0).isEqualTo(SubscriberStatusChanged(subscribersActive = true))
+      index(1).isEqualTo(SubscriberStatusChanged(subscribersActive = false))
+    }
+  }
+
+  @Test fun testDoesNotEmitStaleState() = runFlowTest {
+    val store = stopLightStore()
+
+    val collector = store.testCollector()
+    store.dispatch(SetRedLightOn(false))
+    assertThat(collector.values).all {
+      hasSize(2)
+      index(0).hasDefaultLights()
+      index(1).hasLights()
+    }
+
+    collector.stopCollecting()
+    store.dispatch(SetGreenLightOn(true))
+    assertThat(collector.values).hasSize(2) // no change because not collecting
+    assertThat(store.state).hasLights(green = true)
+
+    collector.startCollecting()
+    assertThat(collector.values).all {
+      hasSize(3)
+      lastElement().hasLights(green = true)
     }
   }
 }
