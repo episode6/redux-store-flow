5a8cf571752f4b809bcea7b797473766bee9a2cc
Geoff Hackett <ghackett@gmail.com>
2022-03-19 13:04:35 -0400

    Add a subscriber aware store flow (#12)
    * start on subscriber aware store flow
    
    * add kdoc and placeholder test
    
    * tests
    
    * BOOO shareIn emits stale data when resubscribing :(
    
    * make sure collectors are never empty
    
    * avoid stale emissions
    
    * change order of operations in flow
    
    * attempt to fix dokkaHtml on github
    
    * add isNotEmpty check to first test

---
+++ subscriber-aware/src/commonMain/kotlin/com/episode6/redux/subscriberaware/SubscriberAwareStoreFlow.kt
@@ -0,0 +1,41 @@
+package com.episode6.redux.subscriberaware
+
+import com.episode6.redux.Action
+import com.episode6.redux.Middleware
+import com.episode6.redux.Reducer
+import com.episode6.redux.StoreFlow
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.*
+
+/**
+ * An [Action] that is dispatched to a [StoreFlow] that is created with [SubscriberAwareStoreFlow]
+ */
+data class SubscriberStatusChanged(val subscribersActive: Boolean = false) : Action
+
+/**
+ * Creates a [StoreFlow] that dispatches [SubscriberStatusChanged] when subscribers start or stop
+ * collecting from it. The actions will fire when the first subscriber starts and the last subscriber
+ * stops collecting.
+ */
+@Suppress("FunctionName") fun <State : Any?> SubscriberAwareStoreFlow(
+  scope: CoroutineScope,
+  initialValue: State,
+  reducer: Reducer<State>,
+  middlewares: List<Middleware<State>> = emptyList(),
+): StoreFlow<State> {
+
+  val store = StoreFlow(scope = scope, initialValue = initialValue, reducer = reducer, middlewares = middlewares)
+
+  val flow = store
+    .onStart { store.dispatch(SubscriberStatusChanged(true)) }
+    .onCompletion { store.dispatch(SubscriberStatusChanged(false)) }
+    .drop(1) // since we have to emit onStart, drop the store's first emission
+    .shareIn(scope, SharingStarted.WhileSubscribed(), replay = 0) // replay = stale emissions when SharingStarted.WhileSubscribed is used
+    .onStart { emit(store.state) } // replacement for replay
+
+  return object : StoreFlow<State>, Flow<State> by flow {
+    override val initialState: State get() = store.initialState
+    override val state: State get() = store.state
+    override fun dispatch(action: Action) = store.dispatch(action)
+  }
+}
---
+++ subscriber-aware/src/commonTest/kotlin/com/episode6/redux/subscriberaware/SubscriberAwareStoreFlowTest.kt
@@ -0,0 +1,128 @@
+package com.episode6.redux.subscriberaware
+
+import assertk.all
+import assertk.assertThat
+import assertk.assertions.*
+import com.episode6.redux.Action
+import com.episode6.redux.Middleware
+import com.episode6.redux.StoreFlow
+import com.episode6.redux.testsupport.lastElement
+import com.episode6.redux.testsupport.runFlowTest
+import com.episode6.redux.testsupport.stoplight.*
+import kotlinx.coroutines.CoroutineScope
+import kotlin.test.Test
+
+class SubscriberAwareStoreFlowTest {
+
+  val actions = mutableListOf<Action>()
+  val middleware = Middleware<StopLightState> { _, next ->
+    {
+      actions += it
+      next(it)
+    }
+  }
+
+  private fun CoroutineScope.stopLightStore(): StoreFlow<StopLightState> = SubscriberAwareStoreFlow(
+    scope = this,
+    initialValue = StopLightState(),
+    reducer = StopLightState::reduce,
+    middlewares = listOf(middleware)
+  )
+
+  @Suppress("UNUSED_VARIABLE")
+  @Test fun testNoInitialAction() = runFlowTest {
+    val store = stopLightStore()
+
+    assertThat(actions).isEmpty()
+  }
+
+  @Test fun testSubscribe() = runFlowTest {
+    val store = stopLightStore()
+
+    store.test {
+      assertThat(actions).containsExactly(SubscriberStatusChanged(subscribersActive = true))
+      assertThat(values).isNotEmpty()
+    }
+  }
+
+  @Test fun testUnSubscribe() = runFlowTest {
+    val store = stopLightStore()
+
+    store.testCollector().stopCollecting()
+
+    assertThat(actions).all {
+      hasSize(2)
+      index(0).isEqualTo(SubscriberStatusChanged(subscribersActive = true))
+      index(1).isEqualTo(SubscriberStatusChanged(subscribersActive = false))
+    }
+  }
+
+  @Test fun testMultipleSubscribers() = runFlowTest {
+    val store = stopLightStore()
+
+    val collector1 = store.testCollector()
+    val collector2 = store.testCollector()
+
+    assertThat(actions).all {
+      hasSize(1)
+      index(0).isEqualTo(SubscriberStatusChanged(subscribersActive = true))
+    }
+    assertThat(collector1.values).isNotEmpty()
+    assertThat(collector2.values).isNotEmpty()
+  }
+
+  @Test fun testDanglingSubscriber() = runFlowTest {
+    val store = stopLightStore()
+
+    val collector1 = store.testCollector()
+    val collector2 = store.testCollector()
+    collector1.stopCollecting()
+
+    assertThat(actions).all {
+      hasSize(1)
+      index(0).isEqualTo(SubscriberStatusChanged(subscribersActive = true))
+    }
+    assertThat(collector1.values).isNotEmpty()
+    assertThat(collector2.values).isNotEmpty()
+  }
+
+  @Test fun testMultipleSubscribers_unsubscribe() = runFlowTest {
+    val store = stopLightStore()
+
+    val collector1 = store.testCollector()
+    val collector2 = store.testCollector()
+    collector1.stopCollecting()
+    collector2.stopCollecting()
+
+    assertThat(actions).all {
+      hasSize(2)
+      index(0).isEqualTo(SubscriberStatusChanged(subscribersActive = true))
+      index(1).isEqualTo(SubscriberStatusChanged(subscribersActive = false))
+    }
+    assertThat(collector1.values).isNotEmpty()
+    assertThat(collector2.values).isNotEmpty()
+  }
+
+  @Test fun testDoesNotEmitStaleState() = runFlowTest {
+    val store = stopLightStore()
+
+    val collector = store.testCollector()
+    store.dispatch(SetRedLightOn(false))
+    assertThat(collector.values).all {
+      hasSize(2)
+      index(0).hasDefaultLights()
+      index(1).hasLights()
+    }
+
+    collector.stopCollecting()
+    store.dispatch(SetGreenLightOn(true))
+    assertThat(collector.values).hasSize(2) // no change because not collecting
+    assertThat(store.state).hasLights(green = true)
+
+    collector.startCollecting()
+    assertThat(collector.values).all {
+      hasSize(3)
+      lastElement().hasLights(green = true)
+    }
+  }
+}
