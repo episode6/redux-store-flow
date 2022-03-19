f312d3fcc4fa2a0712ef72258c9889ae6b8e4af9
Geoff Hackett <ghackett@gmail.com>
2022-03-19 15:34:30 -0400

    move coroutines-test & mockk to jvm-only

---
+++ test-support/src/jsMain/kotlin/com/episode6/redux/testsupport/JsPlatformActual.kt
@@ -0,0 +1,8 @@
+package com.episode6.redux.testsupport
+
+import kotlinx.coroutines.CoroutineScope
+
+actual fun runTest(testBody: suspend CoroutineScope.() -> Unit) {
+  // no-op, coroutine tests are not supported on JS in kotlin 1.5
+  throw AssertionError()
+}
