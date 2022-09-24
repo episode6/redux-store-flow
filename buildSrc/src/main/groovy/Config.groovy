796224ead4d3b57c24d9e0dfdd6dc64566ea6cb7
Geoff Hackett <ghackett@gmail.com>
2022-09-24 16:36:52 -0400

    Add ios targets

--- buildSrc/src/main/groovy/Config.groovy
+++ buildSrc/src/main/groovy/Config.groovy
@@ -18,7 +18,20 @@ class Config {
         "linuxX64",
     ]
     public static String[] apple = [
+        "iosArm32",
+        "iosArm64",
+        "iosX64",
+        "iosSimulatorArm64",
         "macosX64",
+        "macosArm64",
+        "tvosArm64",
+        "tvosX64",
+        "tvosSimulatorArm64",
+        "watchosArm32",
+        "watchosArm64",
+        "watchosX86",
+        "watchosX64",
+        "watchosSimulatorArm64",
     ]
     public static String[] windows = [
         "mingwX64",
