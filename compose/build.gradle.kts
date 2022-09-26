d62cd70093bd02df25cb7a264281f2d431bef124
Geoff Hackett <ghackett@gmail.com>
2022-09-26 18:18:51 -0400

    didn't need android at all, nice

--- compose/build.gradle.kts
+++ compose/build.gradle.kts
@@ -1,55 +1,18 @@
 description = "Jetpack Compose support for Redux StoreFlow"
 
 plugins {
-  id("com.android.library")
   id("config-multi-deploy")
   id("org.jetbrains.compose")
 }
 
 kotlin {
-  android()
-
   sourceSets {
     val commonMain by getting {
       dependencies {
         api(libs.kotlinx.coroutines.core)
-        api(project(":core"))
         api(compose.runtime)
+        api(project(":core"))
       }
-    }
-    val androidMain by getting {
-      dependsOn(commonMain)
-    }
-  }
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-  kotlinOptions.jvmTarget = Config.Jvm.name
-}
-
-android {
-  compileSdk = 31
-
-  defaultConfig {
-    minSdk = 21
-    targetSdk = 31
-  }
-
-  compileOptions {
-    sourceCompatibility = Config.Jvm.sourceCompat
-    targetCompatibility = Config.Jvm.targetCompat
-  }
-
-  sourceSets {
-    named("main") {
-      manifest.srcFile("src/androidMain/AndroidManifest.xml")
-    }
-  }
-
-  publishing {
-    singleVariant("release") {
-      withSourcesJar()
-      withJavadocJar()
     }
   }
 }
