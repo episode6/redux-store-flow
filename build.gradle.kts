8c54aeb56364438c4b583e4e63468291b1c63938
Geoff Hackett <ghackett@gmail.com>
2026-06-18 09:19:02 -0400

    Reapply "TEST MODE 2"
    This reverts commit abc5850fca8df464de44ad313037161e95da035b.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 allprojects {
   group = "com.episode6.redux"
-  version = "1.1.3-SNAPSHOT"
+  version = "1.1.2.1"
 }
 description = "Yet another kotlin implementation of Redux, backed by StateFlows and Coroutines"
 
